31d131b96a03a26abf8108d89058d80a2e6a5a87
olankens <olankens@users.noreply.github.com>
2026-08-17 13:10:26 +0200

    Add REST controller for product CRUD operations

---
+++ product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
@@ -0,0 +1,47 @@
+package com.ecommerce.product_service.controller;
+
+import com.ecommerce.product_service.dto.ProductRequestDTO;
+import com.ecommerce.product_service.dto.ProductResponseDTO;
+import com.ecommerce.product_service.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponseDTO createProduct(@RequestBody ProductRequestDTO productRequest) {
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponseDTO> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ProductResponseDTO getProductById(@PathVariable String id) {
+        return productService.getProductById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable String id) {
+        productService.deleteProduct(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductResponseDTO updateProduct(@PathVariable String id, @RequestBody ProductRequestDTO productRequest) {
+        return productService.updateProductById(id, productRequest);
+    }
+}
